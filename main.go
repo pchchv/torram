9b21e174cbc7d9ff3859fac5d383667e31bd2739
Evgeni Pochechuev <ipchchv@gmail.com>
2026-08-28 06:40:13 +0000

    feat: implement loading of values ​​from the .env file

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
+import (
+	"log"
+
+	"github.com/pchchv/env"
+)
+
+func init() {
+	// Load values from .env into the system
+	if err := env.Load(); err != nil {
+		log.Panic("No .env file found")
+	}
+}
+
 func main() {}
