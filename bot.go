a7c648cfac94a54071c5e47675247f5a8b013e7b
Evgeni Pochechuev <ipchchv@gmail.com>
2026-08-28 07:08:56 +0000

    feat(bot): implement bot initialization

--- bot.go
+++ bot.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 )
 
 func runBot(token string) {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
+	}
+
+	b, err := bot.New(token, opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	b.Start(ctx)
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
