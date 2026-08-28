96353fa43c34ec26d3e690a7a6f36123d7277f7c
Evgeni Pochechuev <ipchchv@gmail.com>
2026-08-28 06:42:33 +0000

    feat: implement getting of env value

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/pchchv/env"
 )
 
 func init() {
-	// Load values from .env into the system
+	// Load values from .env into the system.
 	if err := env.Load(); err != nil {
 		log.Panic("No .env file found")
 	}
+}
+
+func getEnvValue(v string) string {
+	// Getting a value.
+	// Outputs a panic if the value is missing.
+	value, exist := os.LookupEnv(v)
+	if !exist {
+		log.Panicf("Value %v does not exist", v)
+	}
+
+	return value
 }
 
 func main() {}
