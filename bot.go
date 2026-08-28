351ecb093e1b5fa94dd1878c9b3a2933dfdcd553
Evgeni Pochechuev <ipchchv@gmail.com>
2026-08-28 07:08:40 +0000

    feat(bot): add basic handler

--- bot.go
+++ bot.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"context"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
 func runBot(token string) {
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   update.Message.Text,
+	})
 }
